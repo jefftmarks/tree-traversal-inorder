d66d6edfebd8a83ba5a87374347a5fa909d3d569
Jeff Marks <jeff.t.marks@gmail.com>
2022-11-09 07:59:37 -0500

    done with assignment

--- javascript/tree_traversal_inorder.js
+++ javascript/tree_traversal_inorder.js
@@ -6,13 +6,46 @@ class Node {
   }
 }
 
+// function treeTraversalInorder(root, result = []) {
+// 	if (root === null) {
+// 		return [];
+// 	}
+
+// 	treeTraversalInorder(root.left, result);
+// 	result.push(root.value);
+// 	treeTraversalInorder(root.right, result);
+// 	return result;
+// }
+
 function treeTraversalInorder(root) {
-  // type your code here
+	const result = [];
+	const pending = root? [root] : [];
+
+	while (pending.length > 0) {
+		const node = pending[0];
+
+		if (node.left && !node.done) {
+			pending.unshift(node.left);
+			node.done = true;
+			continue;
+		}
+
+		delete node.done;
+		result.push(node.value);
+		pending.shift();
+
+		if (node.right) {
+			pending.unshift(node.right);
+		}
+	}
+
+	return result;
 }
 
 if (require.main === module) {
   // add your own tests in here
   let root = new Node(2, new Node(-10), new Node(20));
+
   console.log("Expecting: [-10, 2, 20]");
   console.log(treeTraversalInorder(root));
 
@@ -30,3 +63,9 @@ module.exports = {
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+/**
+ * 
+ * 
+ * 
+ * 
+ */
